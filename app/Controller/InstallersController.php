fba60043d0e6d7a8682bb7fce8e12ef2c23e6d85
Skyler Lewis <skyler@simplescripts.com>
2012-08-26 18:04:31 -0600

    Added methods to turn of installation availability, as well as a master method to call installers removal methods.

--- app/Controller/InstallersController.php
+++ app/Controller/InstallersController.php
@@ -7,12 +7,41 @@ App::uses('AppController', 'Controller');
  */
 class InstallersController extends AppController {
 
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow();
+	}
+
 /**
  * index method
  *
  * @return void
  */
 	public function index() {
-		Router::connect('/', array('controller' => 'pastes', 'action' => 'index'));
+		//
+	}
+
+/**
+ * removeInstallers method
+ * Sets autoRender to false. No view required.
+ *
+ * @return void
+ */
+	public function removeInstallers() {
+		$this->autoRender = false;
+		//$this->setInstall('0');
+	}
+
+/**
+ * setInstall method
+ * Sets autoRender to false. No view required.
+ *
+ * @return void
+ */
+	public function setInstall($boolean = 0) {
+		$this->autoRender = false;
+		$CoreFile = file_get_contents(APP . 'Config' . DS . 'core.php');
+		$CoreFile = str_replace("'requireInstall', 1)", "'requireInstall', 0)", $CoreFile);
+		file_put_contents(APP . 'Config' . DS . 'core.php', $CoreFile);
 	}
 }
